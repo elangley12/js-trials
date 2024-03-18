405622c784d158dbbedd69d6463b6ea2dfd286e8
Emily <emily.j.langley@gmail.com>
2024-03-18 12:41:27 -0700

    translated truncate() to python

--- trials.py
+++ trials.py
@@ -83,7 +83,14 @@ def longest_word_length(words):
 
 
 def truncate(string):
-    pass  # TODO: replace this line with your code
+    
+    result = []
+
+    for char in string:
+        if len(result) == 0 or char != result[-1]:
+            result.append(char)
+
+    return ''.join(result)
 
 
 def has_balanced_parens(string):
