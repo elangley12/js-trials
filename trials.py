3a850da08c929de2f6bbab0f601b1fca5fc63712
Emily <emily.j.langley@gmail.com>
2024-03-18 12:24:23 -0700

    translated censor_vowels() to python

--- trials.py
+++ trials.py
@@ -50,7 +50,16 @@ def get_range(start, stop):
 
 
 def censor_vowels(word):
-    pass  # TODO: replace this line with your code
+    
+    chars = []
+
+    for letter in word:
+        if letter in "aeiou":
+            chars.append("*")
+        else:
+            chars.append(letter)
+
+    return "".join(chars)
 
 
 def snake_to_camel(string):
